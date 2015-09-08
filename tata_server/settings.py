5902959397b8a07ef400ab75562461d50431319a
Raul Ojeda <rauloojs@gmail.com>
2015-09-07 23:10:53 -0500

    Removed database

--- tata_server/settings.py
+++ tata_server/settings.py
@@ -77,12 +77,6 @@ WSGI_APPLICATION = 'tata_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-    }
-}
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
