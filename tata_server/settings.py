361b69d1697661316a3c778fceb3ecc6c92c6163
Raul Ojeda <rauloojs@gmail.com>
2015-09-07 22:35:08 -0500

    Trying fix

--- tata_server/settings.py
+++ tata_server/settings.py
@@ -100,9 +100,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, '/static/'),)
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+STATIC_ROOT= os.path.join(PROJECT_DIR,'staticfiles/')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT,'static/'),
+)
 
 # # Allow all host hosts/domain names for this site
 ALLOWED_HOSTS = ['*']
