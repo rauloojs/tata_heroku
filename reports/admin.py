3c5cd6c08c32d53b10915bc07729f331755971fd
Raul Ojeda <rauloojs@gmail.com>
2015-09-22 00:32:37 -0500

    Added last reports to admin page

--- reports/admin.py
+++ reports/admin.py
@@ -1,6 +1,8 @@
-from reports.models import Report, PsyReport, NutReport
+from reports.models import Report, PsyReport, NutReport, FisReport, GerReport
 from django.contrib import admin
 
 admin.site.register(Report)
 admin.site.register(PsyReport)
 admin.site.register(NutReport)
+admin.site.register(FisReport)
+admin.site.register(GerReport)
